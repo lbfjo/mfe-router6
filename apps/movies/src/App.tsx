835883f8a5546d637584dd2eb45b5f2776f67cd4
Francisco Bruno <franciscobruno@MBPdeFrancisco.lan>
2022-09-29 20:28:46 +0100

    added shared Packages for both routes

--- apps/movies/src/App.tsx
+++ apps/movies/src/App.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { AppShell } from "ui";
-import { MoviesContent } from "./MoviesContent";
+import { MoviesContent } from "movies-content";
+import { PlaylistContent } from "playlist-content";
+
 function App() {
   return (
     <AppShell
       routes={[
         { path: "/", element: MoviesContent },
-        { path: "/playlist", element: () => <div>Playlist</div> },
+        { path: "/playlist", element: PlaylistContent },
       ]}
       title="Movies"
       colorScheme="light"
       navLinks={[
-        { label: "Home", path: "/home" },
+        { label: "Home", path: "/" },
         { label: "Playlist", path: "/Playlist" },
       ]}
     />
